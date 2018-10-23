3dec8202f8270530ca25ffc9ab326ed3cb930d29
Rachel Pfeiffer <rochelfried9@gmail.com>
2018-10-23 20:49:34 +0300

    rewrote code to be scalable - turned Cat into an object and added event listener that will work on all future cats iyH!

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const catPic = document.querySelector(".image");
-let clickNumber = Number((document.querySelector('.click-number')).innerText);
-let changeThisOnscreen = document.querySelector('.click-number');
-catPic.addEventListener('click', function incrementClicks() {
-  clickNumber += 1;
-  changeThisOnscreen.innerText = clickNumber;
+
+//make a catpictures object with catnames
+//for each catpicture:
+//create a cat container
+//inside, put 3 divs, one with cat name
+//and one with cat picture
+//and one with the click counter
+
+const Cat = function Cat(name, image) {
+  this.name = name;
+  this.image = image;
+  this.clicks = 0;
+}
+
+let staredyCat = new Cat('Staredy', 'img/staredy.jpg');
+let scaredyCat = new Cat('Scaredy', 'img/scaredy.jpg');
+
+function newCatDiv(cat) {
+  let catContainer = document.createElement('div');
+  catContainer.innerHTML = cat.name + '<br></br><img class="'+cat.name+'" src="img/' + cat.name + '.jpg">' ;
+  let mainContainer = document.querySelector('.container');
+  mainContainer.appendChild(catContainer);
+  let catClicker = document.createElement('div');
+  catClicker.innerHTML = '<div class="title-click">Clicks: <span class="'+cat.name+'">'+cat.clicks+'</span></div>';
+  catContainer.appendChild(catClicker);
+};
+
+newCatDiv(staredyCat);
+newCatDiv(scaredyCat);
+document.addEventListener('click', function(e) {
+  let matchingDivs = document.getElementsByClassName(e.target.classList.value);
+  let currentClickNumber = matchingDivs[1].innerText;
+  let newClickNumber = Number(currentClickNumber) + 1;
+  matchingDivs[1].innerText = newClickNumber;
 });
