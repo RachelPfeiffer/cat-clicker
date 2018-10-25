a495140213c8418f8dd77a3051e1491874a2b217
Rachel Pfeiffer <rochelfried9@gmail.com>
2018-10-25 09:41:51 +0300

    begin refactoring with MVO organization

--- app.js
+++ app.js
@@ -1,61 +1,35 @@
-
-//make a catpictures object with catnames
-//for each catpicture:
-//create a cat container
-//inside, put 3 divs, one with cat name
-//and one with cat picture
-//and one with the click counter
-
-const Cat = function Cat(name) {
-  this.name = name;
-  this.imagesource = 'img/'+this.name+'.jpg';
-  this.clicks = 0;
-  this.sidebar = function() {
-    let catMenu = document.querySelector('.sidebar');
-    let newCatName = document.createElement('div');
-    newCatName.classList = this.name;
-    newCatName.classList.add('sidebar-entry');
-    newCatName.innerText = this.name;
-    catMenu.appendChild(newCatName);
-  }
-}
-const catNames = ['Staredy', 'Scaredy'];
-
-for (x of catNames) {
-  console.log(x);
-  let newCat = new Cat(x);
-  console.log(newCat);
-  newCat.sidebar();
-}
-
-function drawCatDiv(cat) {
-  //create new cat div
-  let catContainer = document.createElement('div');
-  catContainer.classList.add(cat);
-  catContainer.classList.add("cat-box");
-  //add name & picture
-  catContainer.innerHTML = cat + '<br></br><img class="cat-picture '+cat+'" src="img/' + cat + '.jpg">' ;
-  let mainContainer = document.querySelector('.container');
-  mainContainer.appendChild(catContainer);
-  //add clicker
-  let catClicker = document.createElement('div');
-  catClicker.innerHTML = '<div class="title-click">Clicks: <span class="'+cat+'">'+cat+'</span></div>';
-  catContainer.appendChild(catClicker);
+//section 1: the model - in which we'll store all the lists of cats,
+//pictures, and click-numbers
+let model = {
+  catNumbers: [0,1,2,3,4],
+  catNames: ['Scaredy', 'Staredy', 'Weirdy', 'Beardy', 'Dirty'],
+  catImages: ['img/cat1.jpg', 'img/cat2.jpg', 'img/cat3.jpg', 'img/cat4.jpg', 'img/cat5.jpg'],
+  catClicks: [0,0,0,0,0]
 };
 
-document.addEventListener('click', function(e) {
-  if (e.target.classList.contains('sidebar-entry')) {
-    if (document.querySelector('.cat-box') !== null) {
-      let catBoxToRemove = document.querySelector('.cat-box');
-      catBoxToRemove.parentElement.removeChild(catBoxToRemove);
-    };
-    drawCatDiv(e.target.classList[0]);
-  } else if (e.target.classList.contains('cat-picture')) {
-    let matchingDivs = document.getElementsByClassName(e.target.classList.value);
-    let currentClickNumber = matchingDivs[1].innerText;
-    let newClickNumber = Number(currentClickNumber) + 1;
-    matchingDivs[1].innerText = newClickNumber;
-  } else {
-    return;
+let octopus = {
+  allCats: function() {
+    let cats = [];
+    function Cat(name,imageURL,clicks) {
+        this.name = name;
+        this.imageURL = imageURL;
+        this.clicks = clicks;
+      }
+      for (number of model.catNumbers) {
+      cats[number] = new Cat(model.catNames[number], model.catImages[number], model.catClicks[number]);
+    }
+    return cats;
   }
-});
+}
+
+let sidebar = {
+  render: function() {
+    for (cat of octopus.allCats()) {
+      let sidebarDiv = document.createElement('div');
+      sidebarDiv.classList.add('.sidebar-entry');
+      let sidebar = document.querySelector('.sidebar');
+      sidebarDiv.innerText = cat.name;
+      sidebar.append(sidebarDiv);
+  }
+}
+}
